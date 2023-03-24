ec11dec32bb430b038e09d92537427d6ea352e91
Earl A. Villapaz <jasonburning16@gmail.com>
2023-03-24 17:41:35 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Content from './components/Content';
+
+const App = () => {
+  return (
+    <div>
+      <Content />
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/iEarlG" target="_blank">Earl A. Villapaz</a>.
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div>
+       <h1>Preview</h1>
+
+Gabrielle Essence Eau De Parfum
+
+A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+Perfumer-Creator for the House of CHANEL.
+
+$149.99
+$169.99
+
+Add to Cart
+    </div>
+  )
+}
+
+export default Content;
